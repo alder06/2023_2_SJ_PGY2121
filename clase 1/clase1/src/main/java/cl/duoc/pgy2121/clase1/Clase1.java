d256207758a79770c816b9483ec9fea9c12ecf77
alan <alanjnlander@outlook.com>
2023-08-19 09:57:24 -0400

    mensaje

---
+++ clase 1/clase1/src/main/java/cl/duoc/pgy2121/clase1/Clase1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cl.duoc.pgy2121.clase1;
+
+/**
+ *
+ * @author Alanj
+ */
+public class Clase1 {
+
+    public static void main(String[] args) {
+        char sexo= 'M';
+            String nombre= "Alan";
+        boolean isValid=false;
+            byte bytes;
+        int edad=33;
+            float decimal=3.10f;
+            double doble= 3.12d;
+            long numeroLargo= 31243124332;
+            short numeroChiquito= 23;
+            
+        System.out.println("Nombre" + nombre);
+        System.out.println("edad"+ edad);
+        System.out.println("activo"+ isValid );
+    }
+}
